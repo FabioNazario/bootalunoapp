495a3c2f52d741b0d19401640ff2b819e6a89b63
FabioNazario <fabio.nazario88@gmail.com>
2020-12-08 09:46:14 -0300

    Tentando fazer o teste sem precisar abrir o chrome

--- src/test/java/br/edu/infnet/bootalunoapp/InsertIT.java
+++ src/test/java/br/edu/infnet/bootalunoapp/InsertIT.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class InsertIT {
 
